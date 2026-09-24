f3c5c67bde3965aa36509923c2a3d7a69f204d1a
RANDI <288767946+itsmemarysy01-sudo@users.noreply.github.com>
2026-09-25 01:31:25 +0800

    Initial commit

---
+++ src/types.deno.ts
@@ -0,0 +1,334 @@
+// deno-lint-ignore-file no-import-prefix
+
+// === Needed imports
+import { basename } from "jsr:@std/path@1.1.2/basename";
+
+import {
+    type ApiMethods as ApiMethodsF,
+    type InputMedia as InputMediaF,
+    type InputMediaAnimation as InputMediaAnimationF,
+    type InputMediaAudio as InputMediaAudioF,
+    type InputMediaDocument as InputMediaDocumentF,
+    type InputMediaLivePhoto as InputMediaLivePhotoF,
+    type InputMediaPhoto as InputMediaPhotoF,
+    type InputMediaSticker as InputMediaStickerF,
+    type InputMediaVideo as InputMediaVideoF,
+    type InputMediaVoiceNote as InputMediaVoiceNoteF,
+    type InputPaidMedia as InputPaidMediaF,
+    type InputPaidMediaLivePhoto as InputPaidMediaLivePhotoF,
+    type InputPaidMediaPhoto as InputPaidMediaPhotoF,
+    type InputPaidMediaVideo as InputPaidMediaVideoF,
+    type InputPollMedia as InputPollMediaF,
+    type InputPollOption as InputPollOptionF,
+    type InputPollOptionMedia as InputPollOptionMediaF,
+    type InputProfilePhoto as InputProfilePhotoAnimatedF,
+    type InputProfilePhoto as InputProfilePhotoF,
+    type InputProfilePhoto as InputProfilePhotoStaticF,
+    type InputRichBlock as InputRichBlockF,
+    type InputRichBlockAnimation as InputRichBlockAnimationF,
+    type InputRichBlockAudio as InputRichBlockAudioF,
+    type InputRichBlockBlockQuotation as InputRichBlockBlockQuotationF,
+    type InputRichBlockCollage as InputRichBlockCollageF,
+    type InputRichBlockDetails as InputRichBlockDetailsF,
+    type InputRichBlockDocument as InputRichBlockDocumentF,
+    type InputRichBlockList as InputRichBlockListF,
+    type InputRichBlockListItem as InputRichBlockListItemF,
+    type InputRichBlockPhoto as InputRichBlockPhotoF,
+    type InputRichBlockSlideshow as InputRichBlockSlideshowF,
+    type InputRichBlockVideo as InputRichBlockVideoF,
+    type InputRichBlockVoiceNote as InputRichBlockVoiceNoteF,
+    type InputRichMessage as InputRichMessageF,
+    type InputRichMessageMedia as InputRichMessageMediaF,
+    type InputSticker as InputStickerF,
+    type InputStoryContent as InputStoryContentF,
+    type InputStoryContentPhoto as InputStoryContentPhotoF,
+    type InputStoryContentVideo as InputStoryContentVideoF,
+    type Opts as OptsF,
+} from "https://cdn.jsdelivr.net/gh/grammyjs/types@5.0.0/mod.ts";
+import { debug as d, isDeno } from "./platform.deno.ts";
+
+const debug = d("grammy:warn");
+
+// === Export all API types
+export * from "https://cdn.jsdelivr.net/gh/grammyjs/types@5.0.0/mod.ts";
+
+/** A value, or a potentially async function supplying that value */
+type MaybeSupplier<T> = T | (() => T | Promise<T>);
+/** Something that looks like a URL. */
+interface URLLike {
+    /**
+     * Identifier of the resource. Must be in a format that can be parsed by the
+     * URL constructor.
+     */
+    url: string;
+}
+
+// === InputFile handling and File augmenting
+/**
+ * An `InputFile` wraps a number of different sources for [sending
+ * files](https://grammy.dev/guide/files#uploading-your-own-files).
+ *
+ * It corresponds to the `InputFile` type in the [Telegram Bot API
+ * Reference](https://core.telegram.org/bots/api#inputfile).
+ */
+export class InputFile {
+    private consumed = false;
+    private readonly fileData: ConstructorParameters<typeof InputFile>[0];
+    /**
+     * Optional name of the constructed `InputFile` instance.
+     *
+     * Check out the
+     * [documentation](https://grammy.dev/guide/files#uploading-your-own-files)
+     * on sending files with `InputFile`.
+     */
+    public readonly filename?: string;
+    /**
+     * Constructs an `InputFile` that can be used in the API to send files.
+     *
+     * @param file A path to a local file or a `Buffer` or a `ReadableStream` that specifies the file data
+     * @param filename Optional name of the file
+     */
+    constructor(
+        file: MaybeSupplier<
+            | string
+            | Blob
+            | Deno.FsFile
+            | Response
+            | URL
+            | URLLike
+            | Uint8Array
+            | ReadableStream<Uint8Array>
+            | Iterable<Uint8Array>
+            | AsyncIterable<Uint8Array>
+        >,
+        filename?: string,
+    ) {
+        this.fileData = file;
+        filename ??= this.guessFilename(file);
+        this.filename = filename;
+        if (
+            typeof file === "string" &&
+            (file.startsWith("http:") || file.startsWith("https:"))
+        ) {
+            debug(
+                `InputFile received the local file path '${file}' that looks like a URL. Is this a mistake?`,
+            );
+        }
+    }
+    private guessFilename(
+        file: ConstructorParameters<typeof InputFile>[0],
+    ): string | undefined {
+        if (typeof file === "string") return basename(file);
+        if ("url" in file) return basename(file.url);
+        if (!(file instanceof URL)) return undefined;
+        if (file.pathname !== "/") {
+            const filename = basename(file.pathname);
+            if (filename) return filename;
+        }
+        return basename(file.hostname);
+    }
+    /**
+     * Internal method. Do not use.
+     *
+     * Converts this instance into a binary representation that can be sent to
+     * the Bot API server in the request body.
+     */
+    async toRaw(): Promise<
+        Uint8Array | Iterable<Uint8Array> | AsyncIterable<Uint8Array>
+    > {
+        if (this.consumed) {
+            throw new Error("Cannot reuse InputFile data source!");
+        }
+        const data = this.fileData;
+        // Handle local files
+        if (typeof data === "string") {
+            if (!isDeno) {
+                throw new Error(
+                    "Reading files by path requires a Deno environment",
+                );
+            }
+            const file = await Deno.open(data);
+            return file.readable[Symbol.asyncIterator]();
+        }
+        if (data instanceof Blob) return data.stream();
+        if (isDenoFile(data)) return data.readable[Symbol.asyncIterator]();
+        // Handle Response objects
+        if (data instanceof Response) {
+            if (data.body === null) throw new Error(`No response body!`);
+            return data.body;
+        }
+        // Handle URL and URLLike objects
+        if (data instanceof URL) return await fetchFile(data);
+        if ("url" in data) return await fetchFile(data.url);
+        // Return buffers as-is
+        if (data instanceof Uint8Array) return data;
+        // Unwrap supplier functions
+        if (typeof data === "function") {
+            return new InputFile(await data()).toRaw();
+        }
+        // Mark streams and iterators as consumed and return them as-is
+        this.consumed = true;
+        return data;
+    }
+    toJSON() {
+        throw new Error("InputFile instances must be sent via grammY");
+    }
+}
+
+async function fetchFile(
+    url: string | URL,
+): Promise<AsyncIterable<Uint8Array>> {
+    const { body } = await fetch(url);
+    if (body === null) {
+        throw new Error(`Download failed, no response body from '${url}'`);
+    }
+    return body[Symbol.asyncIterator]();
+}
+function isDenoFile(data: unknown): data is Deno.FsFile {
+    return isDeno && data instanceof Deno.FsFile;
+}
+
+// === Export InputFile types
+/** Wrapper type to bundle all methods of the Telegram API */
+export type ApiMethods = ApiMethodsF<InputFile>;
+
+/** Utility type providing the argument type for the given method name or `{}` if the method does not take any parameters */
+export type Opts<M extends keyof ApiMethods> = OptsF<InputFile>[M];
+
+/** This object describes a sticker to be added to a sticker set. */
+export type InputSticker = InputStickerF<InputFile>;
+
+/** This object represents the content of a media message to be sent. It should be one of
+
+  - InputMediaAnimation
+  - InputMediaAudio
+  - InputMediaDocument
+  - InputMediaLivePhoto
+  - InputMediaPhoto
+  - InputMediaVideo */
+export type InputMedia = InputMediaF<InputFile>;
+/** Represents an animation file (GIF or H.264/MPEG-4 AVC video without sound) to be sent. */
+export type InputMediaAnimation = InputMediaAnimationF<InputFile>;
+/** Represents an audio file to be treated as music to be sent. */
+export type InputMediaAudio = InputMediaAudioF<InputFile>;
+/** Represents a general file to be sent. */
+export type InputMediaDocument = InputMediaDocumentF<InputFile>;
+/** Represents a live photo to be sent. */
+export type InputMediaLivePhoto = InputMediaLivePhotoF<InputFile>;
+/** Represents a photo to be sent. */
+export type InputMediaPhoto = InputMediaPhotoF<InputFile>;
+/** Represents a sticker file to be sent. */
+export type InputMediaSticker = InputMediaStickerF<InputFile>;
+/** Represents a video to be sent. */
+export type InputMediaVideo = InputMediaVideoF<InputFile>;
+/** Represents a voice message file to be sent. */
+export type InputMediaVoiceNote = InputMediaVoiceNoteF<InputFile>;
+/** This object contains information about one answer option in a poll to send. */
+export type InputPollOption = InputPollOptionF<InputFile>;
+/** This object represents the content of a poll description or a quiz explanation to be sent. It should be one of
+
+- InputMediaAnimation
+- InputMediaAudio
+- InputMediaDocument
+- InputMediaLivePhoto
+- InputMediaLocation
+- InputMediaPhoto
+- InputMediaVenue
+- InputMediaVideo */
+export type InputPollMedia = InputPollMediaF<InputFile>;
+/** This object represents the content of a poll option to be sent. It should be one of
+
+ - InputMediaAnimation
+ - InputMediaLivePhoto
+ - InputMediaLocation
+ - InputMediaPhoto
+ - InputMediaSticker
+ - InputMediaVenue
+ - InputMediaVideo */
+export type InputPollOptionMedia = InputPollOptionMediaF<InputFile>;
+/** This object describes the paid media to be sent. Currently, it can be one of
+
+- InputPaidMediaPhoto
+- InputPaidMediaVideo */
+export type InputPaidMedia = InputPaidMediaF<InputFile>;
+/** The paid media to send is a live photo. */
+export type InputPaidMediaLivePhoto = InputPaidMediaLivePhotoF<InputFile>;
+/** The paid media to send is a photo. */
+export type InputPaidMediaPhoto = InputPaidMediaPhotoF<InputFile>;
+/** The paid media to send is a video. */
+export type InputPaidMediaVideo = InputPaidMediaVideoF<InputFile>;
+/** This object describes a profile photo to set. Currently, it can be one of
+
+- InputProfilePhotoStatic
+- InputProfilePhotoAnimated */
+export type InputProfilePhoto = InputProfilePhotoF<InputFile>;
+/** A static profile photo in the .JPG format. */
+export type InputProfilePhotoStatic = InputProfilePhotoStaticF<InputFile>;
+/** An animated profile photo in the MPEG4 format. */
+export type InputProfilePhotoAnimated = InputProfilePhotoAnimatedF<InputFile>;
+/** This object describes the content of a story to post. Currently, it can be one of
+
+- InputStoryContentPhoto
+- InputStoryContentVideo */
+export type InputStoryContent = InputStoryContentF<InputFile>;
+/** Describes a photo to post as a story. */
+export type InputStoryContentPhoto = InputStoryContentPhotoF<InputFile>;
+/** Describes a video to post as a story. */
+export type InputStoryContentVideo = InputStoryContentVideoF<InputFile>;
+
+/** Describes a rich message to be sent. Exactly one of the fields html, markdown, or blocks must be used. */
+export type InputRichMessage = InputRichMessageF<InputFile>;
+/** Describes a rich message to be sent. Exactly one of the fields html, markdown, or blocks must be used. */
+export type InputRichMessageWithoutUpload = InputRichMessageF<never>;
+/** Describes a media element embedded in an outgoing rich message. */
+export type InputRichMessageMedia = InputRichMessageMediaF<InputFile>;
+/** An item of a list to be sent. */
+export type InputRichBlockListItem = InputRichBlockListItemF<InputFile>;
+/** This object represents a block in a rich formatted message to be sent. Currently, it can be any of the following types:
+
+- InputRichBlockParagraph
+- InputRichBlockSectionHeading
+- InputRichBlockPreformatted
+- InputRichBlockFooter
+- InputRichBlockDivider
+- InputRichBlockMathematicalExpression
+- InputRichBlockAnchor
+- InputRichBlockList
+- InputRichBlockBlockQuotation
+- InputRichBlockPullQuotation
+- InputRichBlockCollage
+- InputRichBlockSlideshow
+- InputRichBlockTable
+- InputRichBlockDetails
+- InputRichBlockMap
+- InputRichBlockAnimation
+- InputRichBlockAudio
+- InputRichBlockPhoto
+- InputRichBlockVideo
+- InputRichBlockVoiceNote
+- InputRichBlockThinking */
+export type InputRichBlock = InputRichBlockF<InputFile>;
+/** A list of blocks, corresponding to the HTML tag \<ul> or \<ol> with multiple nested tags \<li>. */
+export type InputRichBlockList = InputRichBlockListF<InputFile>;
+/** A block quotation, corresponding to the HTML tag \<blockquote>. */
+export type InputRichBlockBlockQuotation = InputRichBlockBlockQuotationF<
+    InputFile
+>;
+/** A collage, corresponding to the custom HTML tag \<tg-collage>. */
+export type InputRichBlockCollage = InputRichBlockCollageF<InputFile>;
+/** A slideshow, corresponding to the custom HTML tag \<tg-slideshow>. */
+export type InputRichBlockSlideshow = InputRichBlockSlideshowF<InputFile>;
+/** An expandable block for details disclosure, corresponding to the HTML tag \<details>. */
+export type InputRichBlockDetails = InputRichBlockDetailsF<InputFile>;
+/** A block with an animation, corresponding to the HTML tag \<video>. */
+export type InputRichBlockAnimation = InputRichBlockAnimationF<InputFile>;
+/** A block with a music file, corresponding to the HTML tag \<audio>. */
+export type InputRichBlockAudio = InputRichBlockAudioF<InputFile>;
+/** A block with a general file, corresponding to the custom HTML tag \<tg-document>. */
+export type InputRichBlockDocument = InputRichBlockDocumentF<InputFile>;
+/** A block with a photo, corresponding to the HTML tag \<img>. */
+export type InputRichBlockPhoto = InputRichBlockPhotoF<InputFile>;
+/** A block with a video, corresponding to the HTML tag \<video>. */
+export type InputRichBlockVideo = InputRichBlockVideoF<InputFile>;
+/** A block with a voice note, corresponding to the HTML tag \<audio>. */
+export type InputRichBlockVoiceNote = InputRichBlockVoiceNoteF<InputFile>;
